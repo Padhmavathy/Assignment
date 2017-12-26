30c79f1c02abf195fc07ac06f096020a5ad1704c
padhcs <padhcs@DIN76000820>
2017-12-26 18:08:59 +0530

    Add project files.

---
+++ MVCDemo/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Laptop()
+        {
+            return View();
+        }
+        public ActionResult TV()
+        {
+            return View();
+        }
+        public ActionResult Mobile()
+        {
+            return View();
+        }
+        public ActionResult Microwave()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Controllers/ServicesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult LaptopServices()
+        {
+            return View();
+        }
+        public ActionResult TVServices()
+        {
+            return View();
+        }
+        public ActionResult MobileServices()
+        {
+            return View();
+        }
+        public ActionResult MirowaveServices()
+        {
+            return View();
+        }
+    }
+}
